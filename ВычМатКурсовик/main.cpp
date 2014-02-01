5dc24a167ad4c2bca386e4fdff6127c4e57d1a03
OpenShift guest <poriesto456@gmail.com>
2014-02-01 07:16:52 -0500

    some issue for reverse matrix A = LU

--- ВычМатКурсовик/main.cpp
+++ ВычМатКурсовик/main.cpp
@@ -8,7 +8,7 @@ int main(){
     
 	std::vector<std::vector<double> > A(ReadFromFile(FILE_MATRIX_A));
 	std::vector<double>B = ReadFromFile(FILE_MATRIX_B, 0);
-	std::vector<std::vector<double> >L(A.size()), U(A.size());
+	std::vector<std::vector<double> >L(A.size()), U(A.size()), I(A.size()), revLU(A.size()), revL(A.size()), revA(A.size());
 
 	LU(A,L,U);
     std::cout << "Matrix A" << std::endl;
@@ -28,6 +28,30 @@ int main(){
 	show(Isolve(L,B));
 	std::cout << "decision Ux = y" << std::endl;
 	show(Isolve(U, Isolve(L,B)));
-
+    
+    std::cout << "Find reverse matrix for matrix A" << std::endl;
+    // AX = I, LUX = I, LY = I, UX = Y
+    for(int i = 0; i < (int)A.size(); i++){
+        for(int j = 0; j < (int)A.size(); j++){
+            I[i].push_back(0);
+            revL[i].push_back(0);
+            revA[i].push_back(0);
+        }
+    }
+    for(int i = 0; i < (int)A.size(); i++){
+            I[i][i] = 1;
+    }
+    std::cout << "Ed matrix:" << std::endl;
+    show(I);
+    
+    for(int i = 0; i < 2; i++){
+        revL[i] = Isolve(L,I[i]);
+        revLU[i]= Isolve(U, revL[i]);
+        revA[i] = Isolve(A, I[i]);
+    }
+    std::cout << "rev matrixA:" << std::endl;
+    show(Transpose_Matrix(revA));
+    std::cout << "rev matrixLU:" << std::endl;
+    show(Transpose_Matrix(revLU));
     return 0;
 }
